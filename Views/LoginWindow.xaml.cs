adaf947c235dfb217872804eb5df579f78bb5e42
Jung Woon Jik <dnswlrsla@gmail.com>
2026-05-08 17:12:04 +0900

    feat: LoginWindow UI

---
+++ Views/LoginWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace isegoria_wpf.Views
+{
+    /// <summary>
+    /// LoginWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+
+            this.MouseLeftButtonDown += (s, e) =>
+            {
+                if (e.ButtonState == MouseButtonState.Pressed)
+                    this.DragMove();
+            };
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+            => this.Close();
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameInput.Text;
+            string password = PasswordInput.Password;
+
+            //@TODO 로그인 로직
+            Debug.WriteLine("username : " + username + "\npassword : " + password);
+        }
+
+        private void FindId_Click(object sender, RoutedEventArgs e)
+        {
+            //@TODO: 아이디 찾기 창 열기
+
+        }
+
+        private void FindPassword_Click(object sender, RoutedEventArgs e)
+        {
+            //@TODO: 비밀번호 찾기 창 열기
+        }
+    }
+}
